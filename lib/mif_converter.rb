5dd5451c75bb64e494c7656429fba137f044ea42
4-dash <120916864+4-dash@users.noreply.github.com>
2026-07-28 09:48:57 +0200

    initial commit

---
+++ lib/mif_converter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mif_converter/version"
+
+module MifConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mif_converter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MifConverter do
+  it "has a version number" do
+    expect(MifConverter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
